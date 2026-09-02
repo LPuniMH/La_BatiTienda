95bb138ab7f492342352300cee7c467612c06e18
LPuniMH <lu.ahumadae@duocuc.cl>
2026-09-02 12:47:39 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ function renderizarProductos(listaProductos) {
 
         caja.innerHTML = `
             <div class="card-visual">
-                <span class="card-icono">${obtenerInicial(nombre.producto)}</span>
+                <span class="card-icono">${obtenerInicial(producto.nombre)}</span>
                 <span class="card-codigo">${producto.codigo}</span>
             </div>
             <p class="card-pie-foto">Foto próximamente</p>
