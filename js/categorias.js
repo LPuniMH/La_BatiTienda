8f1e014944d5effce25ed5bd442d06acc46d5b7b
LPuniMH <lu.ahumadae@duocuc.cl>
2026-09-02 20:58:30 -0400

    Update categorias.js

--- js/categorias.js
+++ js/categorias.js
@@ -1,5 +1,7 @@
 const mapaCategorias = {
-    "Figuras": ["FG001", "FG002", "SH001", "SH002", "SH003"],
-    "Peluches": ["PL001", "PL002", "KU001"],
-    "Cartas": ["PO001"]
+    "Figuras": ["FG001", "FG001", "SH001", "SH002", "SH003", "FP345", "FP346"],
+    "Peluches": ["KU001", "PL001", "PL002"],
+    "Cartas": ["PO001"],
+    "Libros": ["MG001", "MG003"],
+    "Otros": ["KU002", "KU003", "AC001", "EA001", "LL034"]
 };
