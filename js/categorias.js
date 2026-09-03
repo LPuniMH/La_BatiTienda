2763619367fdcf8582f036b506874a23f8862249
LPuniMH <lu.ahumadae@duocuc.cl>
2026-09-02 21:20:40 -0400

    Update categorias.js

--- js/categorias.js
+++ js/categorias.js
@@ -1,7 +1,32 @@
 const mapaCategorias = {
-    "Figuras": ["FG001", "FG001", "SH001", "SH002", "SH003", "FP345", "FP346"],
-    "Peluches": ["KU001", "PL001", "PL002"],
-    "Cartas": ["PO001"],
-    "Libros": ["MG001", "MG003"],
-    "Otros": ["KU002", "KU003", "AC001", "EA001", "LL034"]
+    "Figuras": {
+        codigos: ["FG001", "FG001", "SH001", "SH002", "SH003", "FP345", "FP346", "FP348"],
+        titulo: "Figuras de Colección",
+        descripcion: "Encuentra tus personajes favoritos con los mejores detalles."
+    },
+
+    "Peluches": {
+        codigos: ["KU001", "PL001", "PL002"],
+        titulo: "Peluches Adorables",
+        descripcion: "Los peluches más suaves y tiernos de tus franquicias favoritas."
+    },
+
+    "Juegos": {
+        codigos: ["PO001", "MM096"],
+        titulo: "Juegos",
+        descripcion: "Juegos de Mesa y Cartas Coleccionables"
+    },
+
+    "Libros": {
+        codigos: ["MG001", "MG003"],
+        titulo: "Libros",
+        descripcion: ""
+    },
+
+    "Otros": {
+        codigos: ["KU002", "KU003", "AC001", "EA001", "LL034", "MQ8756", "WE138", "LP256"],
+        titulo: "Otros Artículos",
+        descripcion: ""
+    }
+
 };
